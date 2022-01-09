5de41ccacb52040d1815d0a23674a3f0b4518225
floaterest <56704092+floaterest@users.noreply.github.com>
2022-01-09 08:31:04 -0500

    style(Main): decrease SW min width

--- src/Main.svelte
+++ src/Main.svelte
@@ -17,7 +17,7 @@
         display: grid;
         gap: 1rem;
         font-size: 3rem;
-        grid-template-columns:repeat(auto-fill, minmax(300px, 1fr));
+        grid-template-columns:repeat(auto-fill, minmax(270px, 1fr));
     }
 
     main{
