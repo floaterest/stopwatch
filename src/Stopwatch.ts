c9f50c3b928c5f557c72944ded8cfbe1295373f5
floaterest <56704092+floaterest@users.noreply.github.com>
2022-01-13 13:15:30 -0500

    refactor(Stopwatch.ts): add comments

--- src/Stopwatch.ts
+++ src/Stopwatch.ts
@@ -1,7 +1,19 @@
 export default interface Stopwatch{
+    /*
+    must be unique across all stopwatches
+     */
     title: string
+    /*
+    last time when stopwatch is started in unix time(ms)
+     */
     timestamp: number
+    /*
+    number of seconds since the stopwatch is started
+     */
     seconds: number
     started: boolean
+    /*
+    time displayed in the ui
+     */
     time: string
 }
