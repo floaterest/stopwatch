6ddfc43dab1c82daa3e5bade79a5516a5d5421f5
floaterest <56704092+floaterest@users.noreply.github.com>
2022-01-21 14:35:52 -0500

    feat(Nav): add 'add stopwatch' button

--- src/Nav.svelte
+++ src/Nav.svelte
@@ -29,6 +29,16 @@
             </a>
         </li>
         <li>
+            <button>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                     class="bi bi-plus-circle" viewBox="0 0 16 16">
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                </svg>
+                <span>Add Stopwatch</span>
+            </button>
+        </li>
+        <li>
             <a href="https://github.com/Floaterest/">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github"
                      viewBox="0 0 16 16">
@@ -67,25 +77,33 @@
 				letter-spacing: 0.3ch;
 			}
 		}
-	}
 
-	a, .logo{
-		display: flex;
-		align-items: center;
-		height: @nav-size;
-		padding: 0 @space;
-		// text
-		span{
-			max-width: 0;
-			overflow: hidden;
-			white-space: nowrap;
+        // no special styles for button
+		button{
+			border: none;
+			background-color: unset;
+			color: unset;
 		}
 
-		// icon
-		svg{
-			width: @icon-size;
-			height: @icon-size;
-			margin: 0 @space;
+		li > *{
+
+			display: flex;
+			align-items: center;
+			height: @nav-size;
+			padding: 0 @space;
+			// text
+			span{
+				max-width: 0;
+				overflow: hidden;
+				white-space: nowrap;
+			}
+
+			// icon
+			svg{
+				width: @icon-size;
+				height: @icon-size;
+				margin: 0 @space;
+			}
 		}
 	}
 
