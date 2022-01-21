66afbdebe14a02c6e48031e847ded9d32049dcf0
Floaterest <56704092+Floaterest@users.noreply.github.com>
2022-01-21 15:04:58 -0500

    feat(Main,Nav): add 'StopwatchAdd' component

--- src/Nav.svelte
+++ src/Nav.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import { addStopwatch } from './stores';
+
     let expanded = false;
 
     function toggleExpand(){
@@ -29,7 +31,7 @@
             </a>
         </li>
         <li>
-            <button>
+            <button on:click={addStopwatch.update(add => !add)}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                      class="bi bi-plus-circle" viewBox="0 0 16 16">
                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
@@ -78,7 +80,7 @@
 			}
 		}
 
-        // no special styles for button
+		// no special styles for button
 		button{
 			border: none;
 			background-color: unset;
