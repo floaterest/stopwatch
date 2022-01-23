a5a9e2cf84639d41060a46258fb9158cb0b5db9c
floaterest <56704092+floaterest@users.noreply.github.com>
2022-01-23 16:45:50 -0500

    feat(stores.ts, SwList): stopwatches data now is stored in stores

--- src/StopwatchList.svelte
+++ src/StopwatchList.svelte
@@ -1,19 +1,23 @@
 <script lang="ts">
     import StopwatchItem from './StopwatchItem.svelte';
+
     import type Stopwatch from './Stopwatch';
 
-    // create sample stopwatches
-    const titles = Array.from(Array(8), (_, i) => i).map(i => i.toString().padStart(3, '0'));
-    let stopwatches: Stopwatch[] = titles.map(t => {
-        return {
-            title: t,
-            timestamp: new Date().getTime(),
-            seconds: 0,
-            started: false,
-            time: time(0),
-        };
-    });
+    import { stopwatches } from './stores';
 
+    // create sample stopwatches
+    if(!$stopwatches.length){
+        const titles = Array.from(Array(8), (_, i) => i).map(i => i.toString().padStart(3, '0'));
+        stopwatches.set(titles.map(t => {
+            return {
+                title: t,
+                timestamp: new Date().getTime(),
+                seconds: 0,
+                started: false,
+                time: time(0),
+            };
+        }));
+    }
     let interval: number;
     let counting = 0;
     let doubleClick: string;
@@ -47,12 +51,12 @@
      */
     function update(){
         now = new Date().getTime();
-        stopwatches = stopwatches.map(sw => {
+        stopwatches.update(sws => sws.map(sw => {
             if(sw.started){
                 sw.time = time(sw.seconds + delta(sw.timestamp));
             }
             return sw;
-        });
+        }));
     }
 
     /**
@@ -63,13 +67,13 @@
         // if this click is the second click from a double click
         if(doubleClick == sw.title){
             // pause all stopwatches
-            stopwatches = stopwatches.map(sw => {
+            stopwatches.update(sws => sws.map(sw => {
                 if(sw.started){
                     sw.started = false;
                     sw.seconds += delta(sw.timestamp);
                 }
                 return sw;
-            });
+            }));
             // reset to 0 counting stopwatches and clear interval
             console.debug('cleared interval after double click');
             clearInterval(interval);
@@ -109,12 +113,12 @@
         setTimeout(() => doubleClick = null, 500);
 
         // update the clicked stopwatch
-        stopwatches = stopwatches.map(sw => sw.title === sw.title ? sw : sw);
+        stopwatches.update(sws => sws.map(sw => sw.title === sw.title ? sw : sw));
     }
 </script>
 
 <section>
-    {#each stopwatches as stopwatch}
+    {#each $stopwatches as stopwatch}
         <StopwatchItem {stopwatch} {onClick}/>
     {/each}
 </section>
