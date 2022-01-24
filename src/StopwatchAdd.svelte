4349339d8c4547311f1497963b12f7cea46d4391
floaterest <56704092+floaterest@users.noreply.github.com>
2022-01-24 11:09:50 -0500

    feat(SwAdd): works

--- src/StopwatchAdd.svelte
+++ src/StopwatchAdd.svelte
@@ -1,11 +1,29 @@
+<script lang="ts">
+    import { stopwatches } from './stores';
+
+    let target: HTMLInputElement;
+
+    function create(e: FocusEvent){
+        if(!(target = e.target as HTMLInputElement).value) return;
+        stopwatches.update(sws => [
+            ...sws, {
+                title: target.value,
+                timestamp: new Date().getTime(),
+                seconds: 0,
+                started: false,
+                time: stopwatches.time(0),
+            },
+        ]);
+        console.debug(`created ${target.value}`);
+    }
+</script>
+
 <label>
-    <input type="text" placeholder="please work">
+    <input type="text" on:focusout={create} placeholder="type title here">
 </label>
 
 <style>
     input{
         font-size: 1rem;
-        width:8ch;
-        height:100%;
     }
 </style>
