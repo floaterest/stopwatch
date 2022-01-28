04a6564705bdfe04e5e350b796602f1733b57c8c
floaterest <56704092+floaterest@users.noreply.github.com>
2022-01-28 17:43:43 -0500

    style(SwAdd): center element by adding style to main

--- src/StopwatchAdd.svelte
+++ src/StopwatchAdd.svelte
@@ -67,6 +67,13 @@
 </section>
 
 <style>
+    :global(main){
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+    }
+
     input{
         font-size: 1rem;
     }
