4c215ca1cc206b5eb32f549873530ab7782d3f5d
Floaterest <56704092+Floaterest@users.noreply.github.com>
2022-02-01 18:56:00 -0500

    refactor(SwList): add SwInput

---
+++ src/StopwatchInput.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    import type Stopwatch from './Stopwatch';
+
+    import { stopwatches } from './stores';
+</script>
+<form>
+    <label for="add"></label>
+    <input id="add" type="text" placeholder="type title here">
+    <button>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+             class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
+        </svg>
+    </button>
+</form>
+
+<style lang="less">
+	form{
+		width: 100%;
+		display: flex;
+		justify-content: space-evenly;
+		border-radius: 1rem;
+		border: 1px solid white;
+
+		input{
+			margin: 0 10px;
+			outline: none;
+			background: none;
+			border: none;
+			font-size: 1rem;
+			width: 100%;
+
+			&::placeholder{
+				color: var(--text-color);
+			}
+		}
+	}
+</style>
--- src/StopwatchList.svelte
+++ src/StopwatchList.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import StopwatchInput from './StopwatchInput.svelte';
     import StopwatchItem from './StopwatchItem.svelte';
 
     import type Stopwatch from './Stopwatch';
@@ -106,50 +107,19 @@
     }
 </script>
 
-<form>
-    <label for="add"></label>
-    <input id="add" type="text" placeholder="type title here">
-    <button>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-             class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
-            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
-        </svg>
-    </button>
-</form>
+<StopwatchInput/>
 <section>
     {#each $stopwatches as stopwatch}
         <StopwatchItem {stopwatch} {onClick}/>
     {/each}
 </section>
 
-<style lang="less">
-	form{
-		width: 100%;
-		display: flex;
-		justify-content: space-evenly;
-		border-radius: 1rem;
-		border: 1px solid white;
-
-		input{
-			margin: 0 10px;
-			outline: none;
-			background: none;
-			border: none;
-			font-size: 1rem;
-			width: 100%;
-
-			&::placeholder{
-				color: var(--text-color);
-			}
-		}
-	}
-
-
-	section{
-		width: 100%;
-		display: grid;
-		gap: 1rem;
-		font-size: 3rem;
-		grid-template-columns:repeat(auto-fill, minmax(270px, 1fr));
-	}
+<style>
+    section{
+        width: 100%;
+        display: grid;
+        gap: 1rem;
+        font-size: 3rem;
+        grid-template-columns:repeat(auto-fill, minmax(270px, 1fr));
+    }
 </style>
