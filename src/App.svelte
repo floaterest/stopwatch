b7fe870b3a8080876b2ecb1e186a98f833deaedf
Floaterest <floaterest@outlook.jp>
2022-06-30 13:07:10 -0400

    redo

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,37 @@
 <script lang="ts">
-    import Nav from './Nav.svelte';
-    import Main from './Main.svelte';
+	import List from './lib/List.svelte';
+	import Input from './lib/Input.svelte';
+	import { now, stopwatches, times } from './lib/stores';
+	import type { Stopwatch } from './lib/Stopwatch.ts';
+
+	/// key for localStorage
+	export let key: string;
+
+	// load times from localStorage
+	function load(){
+		let storage;
+		if(!(storage = localStorage.getItem(key))) return;
+		const timestamp = $now;
+		const started = false;
+		$stopwatches = Object.entries(JSON.parse(storage) as { [id: string]: number }).reduce(
+			(acc, [title, duration]) => {
+				acc[title] = {
+					started,
+					timestamp,
+					duration,
+				} as Stopwatch;
+				return acc;
+			},
+			{}
+		);
+	}
+
+	// save times to localStorage
+	function unload(){
+		localStorage.setItem(key, JSON.stringify($times));
+	}
 </script>
 
-<Nav/>
-<Main/>
-
+<svelte:window on:beforeunload={unload} on:load={load}/>
+<Input/>
+<List/>
---
+++ src/lib/stores.ts
@@ -0,0 +1,31 @@
+import { derived, readable, writable } from 'svelte/store';
+import type { Stopwatch } from './Stopwatch';
+
+// current time in seconds
+export const now = readable<number>(new Date().getTime() / 1000 | 0, set => {
+	const interval = setInterval(() => set(new Date().getTime() / 1000 | 0), 1000);
+	return () => clearInterval(interval);
+});
+
+const timestamp = new Date().getTime() / 1000 | 0;
+const started = false;
+const duration = 0;
+// stopwatch title -> stopwatch
+export const stopwatches = writable<{ [id: string]: Stopwatch }>({
+	a: { started, timestamp, duration },
+	b: { started, timestamp, duration },
+	c: { started, timestamp, duration },
+	d: { started, timestamp, duration },
+});
+
+// stopwatch title -> current number of seconds
+export const times = derived<any[], { [id: string]: number }>([now, stopwatches],
+	([$now, $stopwatches]) => Object.entries($stopwatches).reduce(
+		(acc, [title, { started, timestamp, duration }]) => {
+			// if started { add delta } else { use duration }
+			acc[title] = started ? duration + $now - timestamp : duration;
+			return acc;
+		},
+		{}
+	)
+);
