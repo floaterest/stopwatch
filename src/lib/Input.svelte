f402e79c3bcbf42272399603a20e5a14a956cbe4
Floaterest <floaterest@outlook.jp>
2022-07-01 19:43:53 -0400

    fix(input) form sumbit

--- src/lib/Input.svelte
+++ src/lib/Input.svelte
@@ -5,48 +5,42 @@
 	let input = '';
 	let error = '';
 
-	function check(): string{
-		let titles = input.split(' ');
-		for(const t of titles){
-			if(!t) return 'Title cannot be empty string or end with space';
-			if(t in $stopwatches) return t + ' already exists';
-		}
-		return '';
+	function check(){
+		return error = (() => {
+			for(const t of input.split(' ')){
+				if(!t) return 'Title cannot be empty string or end with space';
+				if(t in $stopwatches) return t + ' already exists';
+			}
+			return '';
+		})();
 	}
 
 	function submit(){
-		if(!check()) return input;
+		if(check()) return;
 		const started = false;
 		const timestamp = $now;
 		const duration = 0;
 		for(const t of input.split(' ')){
 			$stopwatches[t] = { started, timestamp, duration } as Stopwatch;
 		}
-		return '';
+		input = '';
 	}
 </script>
 
-<form on:sumbit|preventDefault={()=>input=submit()} class:error>
-    <label for="add"></label>
-    <input type="text" id="add" placeholder="type titles here"
-           bind:value={input} on:keyup={()=>error=check()}>
-</form>
+<input type="text" placeholder="type titles here" class:error
+       bind:value={input} on:keyup={e=>(e.keyCode===13?submit:check)()}>
 <span>{error}</span>
 
 <style lang="sass">
     @import '../global'
-    form
-        width: 100%
-        display: flex
+    input
         border-radius: 3em
         border: 1px solid $white
-        &.error
-            border-color: $pink
-    input
         background: unset
         color: unset
         width: 100%
-        border: none
         padding: 1em
         height: 1em
+        &.error
+            border-color: $pink
 </style>
