313aee16a3fa8c049b58a66ae5c5b3f6ca65c933
floaterest <56704092+floaterest@users.noreply.github.com>
2022-09-04 15:42:49 -0400

    refactor(Started) remove unused exports

--- src/lib/Started.svelte
+++ src/lib/Started.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
     import Stopwatch from './Stopwatch.svelte';
     import { now } from './stores';
-
-    export let stopwatch: Stopwatch;
 </script>
 
 <Stopwatch {...$$props} now="{$now}"/>
