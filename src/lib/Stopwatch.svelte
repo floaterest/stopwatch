f7df67b266f2d030df2cbb48d3d6ebd3e6a5043d
floaterest <56704092+floaterest@users.noreply.github.com>
2022-09-06 12:19:47 -0400

    feat(Sw) edit

--- src/lib/Stopwatch.svelte
+++ src/lib/Stopwatch.svelte
@@ -8,11 +8,17 @@
     export let name: string;
     export let on: () => void;
     export let off: () => void;
-    const start = $started.has(name);
-    $: display = seconds(stopwatch, $storage.increment, now);
+
+    let contenteditable = false;
+
+    function focusout({ target }: { target: HTMLElement }){
+        console.log(name, 'focusout', target.innerText);
+        contenteditable = false;
+    }
 
     function edit(){
         console.log(name, 'edit');
+        contenteditable = true;
     }
 
     function remove(){
@@ -20,16 +26,21 @@
         const { [name]: _, ...stopwatches } = $storage.stopwatches;
         $storage.stopwatches = stopwatches as { [name: string]: Stopwatch };
     }
+
+    const start = $started.has(name);
+    $: display = seconds(stopwatch, $storage.increment, now);
 </script>
 
 <fieldset class:start>
     <legend>{name}</legend>
-    <code>{hhmmss(display)}</code>
+    <code {contenteditable} on:focusout={focusout}>{hhmmss(display)}</code>
     <section>
         <button on:click={() => (start?off:on)(display)} class="material-icons">
             {#if start}pause{:else}play_arrow{/if}
         </button>
-        <button on:click={edit} class="material-icons">edit</button>
+        <button on:click={edit} class:disabled={start} class="material-icons">
+            edit
+        </button>
         <button on:click={remove} class="material-icons">delete</button>
     </section>
 </fieldset>
@@ -37,6 +48,11 @@
 <style lang="sass">
     @use '../app' as *
     @use 'sass:color'
+    button.disabled
+        filter: brightness(50%)
+    .start
+        border-color: $lime
+        color: $lime
     fieldset
         display: flex
         flex-direction: column
@@ -46,9 +62,6 @@
         padding: 0
     legend
         margin-left: 1em
-    .start
-        border-color: $lime
-        color: $lime
     code
         width: 100%
         text-align: center
@@ -63,5 +76,6 @@
         background: $darkest
         color: unset
         border: none
-        cursor: pointer
+        &:not(.disabled)
+            cursor: pointer
 </style>
